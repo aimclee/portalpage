d96d7456df8f945ea9c817dc8eb0a4d22ab3ec20
everyvn <karutian@yahoo.com>
2020-04-19 17:10:45 +0700

    second commit

--- portalpage/crawling.py
+++ portalpage/crawling.py
@@ -1,40 +1,72 @@
 import requests
-import feedparser
 from bs4 import BeautifulSoup
 import urllib.request as req
 import ssl
+from bs4.builder import builder_registry
 
 # SSL인증코드
 if hasattr(ssl, '_create_unverified_context'):
   ssl._create_default_https_context = ssl._create_unverified_context
 
-# url 모음
-vnexpress = 'https://vnexpress.net/rss/tin-moi-nhat.rss' #https://vnexpress.net/rss
-thanhnien = 'https://thanhnien.vn/rss/home.rss' #https://thanhnien.vn/rss.html
-tuoitre = 'https://tuoitre.vn/rss/tin-moi-nhat.rss' #https://tuoitre.vn/rss.htm
-dantri = 'https://dantri.com.vn/trangchu.rss' #https://dantri.com.vn/rss.htm
-nhandan = '' #https://www.nhandan.org.vn/rss
-
-def searchTrend():
-  #RSS를 lxml parser로 파싱
-  base_url = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=VN'
-  req = requests.get(base_url)
+def urlparsing(base_url):
+  url = base_url
+  req = requests.get(url)
   html = req.content
-  soup = BeautifulSoup(html, 'lxml') 
+  soup = BeautifulSoup(html, 'lxml')
+  return soup
+
+def news_searching(base_url):
+  soup = urlparsing(base_url)
+  items = soup.find_all('item title')
+
+  #데이터 찾기 (title, img)
+  news_item = soup.find_all('title')
+  image = soup.select('description img[src]')
+  link = soup.find_all('link')
+  news_title = []
+  news_img = []
+  news_link = []
   
-  #데이터 찾기 (title, traffic)
+  for i in news_item:
+    a = i.get_text()
+    news_title.append(a)
+
+  for i in image:
+    a = i['src']
+    news_img.append(a)
+
+  for i in link:
+    a = i.get_text()
+    news_link.append(a)
+    
+  return news_title, news_img, news_link
+
+def searchTrend(base_url):
+  soup = urlparsing(base_url)
+  
+  # 데이터 찾기 (title, traffic)
   search_trend = soup.find_all('title')
   search_traffic = soup.find_all('ht:approx_traffic')
-
-  #배열을 만들자
-  dick = {}
+  # 배열을 만들자
+  trendList = []
+  trafficList = []
   for i in search_trend:
     a = i.get_text()
+    trendList.append(a)
     for j in search_traffic:
-      dick[a] = j.get_text()
-  
-  return dick
+      b = j.get_text()
+      trafficList.append(b)
+  trendList = trendList[1:11]
+  trafficList = trafficList[0:10]
+  merged = dict([x for x in zip(trendList, trafficList)])
+  return merged
 
-a = searchTrend()
-print(a)
+
+def search_news(news):
+  title = news_searching(news)
+  return title
+
+# a = searchTrend('https://trends.google.com/trends/trendingsearches/daily/rss?geo=VN')
+b = search_news('https://vnexpress.net/rss/tin-moi-nhat.rss')
+print(b)
 
--- portalpage/views.py
+++ portalpage/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
-from .crawling import searchTrend
+from django.shortcuts import render, redirect
+import requests
+from .crawling import searchTrend, search_news
 
 # Create your views here.
 
+# rss 모음
+googletrend = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=VN'
+vnexpress = 'https://vnexpress.net/rss/tin-moi-nhat.rss' #https://vnexpress.net/rss
+thanhnien = 'https://thanhnien.vn/rss/home.rss' #https://thanhnien.vn/rss.html
+tuoitre = 'https://tuoitre.vn/rss/tin-moi-nhat.rss' #https://tuoitre.vn/rss.htm
+vtc = 'https://vtc.vn/rss/feed.rss' #https://www.nhandan.org.vn/rss
+baotintuc = 'https://baotintuc.vn/tin-moi-nhat.rss'
+baodautu = 'https://baodautu.vn/trang-chu.rss'
+forbesvietnam = 'https://forbesvietnam.com.vn/rss/home.rss'
+
 def home(request):
-    keyword = searchTrend()
-    return render(request, 'home.html', {'keyword':keyword})
+    # 구글 검색 (CSRF 오류남)
+    if request.method == 'POST':
+        keyword = ' ' #이부분에 뭐가 들어가야 html에서 입력한 값이 들어오나요?
+        queryString = {'q': keyword}
+        searchEngine = 'https://www.google.com/search'
+        r = requests.get(searchEngine, params = queryString)
+        return redirect(r.url)
+
+    trends = searchTrend(googletrend)
+    print(trends)
+    news_vnexpress = search_news(vnexpress)
+    news_thanhnien = search_news(thanhnien)
+    
+    return render(request, 'home.html', {'trends':trends, 'vnexpress':news_vnexpress, 'thanhnien':news_thanhnien})
