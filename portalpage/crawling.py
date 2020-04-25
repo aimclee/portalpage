9252b44f4823e14088965331ce6ad642f266ce5c
everyvn <karutian@yahoo.com>
2020-04-25 13:22:20 +0700

    jottetta

--- portalpage/crawling.py
+++ portalpage/crawling.py
@@ -17,125 +17,53 @@ def urlparsing(base_url):
   return soup
 
 # 뉴스 스크랩 그루핑함수
-def news_merge(main_title, main_image, main_link, sub_items):
-  news_title = [main_title]
-  news_img = [main_image]
-  news_link = [main_link]
-
-  for news in sub_items:
-    news_title.append(news['title'])
-    news_img.append('')
-    news_link.append(news['href'])
-  
-  titleList = news_title[0:8]
-  imgList = news_img[0:8]
-  linkList = news_link[0:8]
+def news_merge(news_title, news_img, news_link):
+  # 리스트자르기
+  titleList = news_title[0:12]
+  imgList = news_img[0:12]
+  linkList = news_link[0:12]
 
   merged = dict([x for x in zip(titleList, zip(linkList, imgList))])
-  return merged
-
-# VNExpress 서칭
-def vnExpress():
-  url = 'https://vnexpress.net/'
-  req = requests.get(url)
-  html = req.content
-  soup = BeautifulSoup(html, 'html.parser')
-
-  # 첫번째 기사 스크랩
-  main_title = soup.select_one(".thumb")['title']
-  main_image = soup.select_one(".thumb img")['src']
-  main_link = soup.select_one(".thumb")['href']
-  
-  # 서브 기사 스크랩
-  sub_items = soup.select('h3.title-news > a')
-  merged = news_merge(main_title, main_image, main_link, sub_items)
   return merged
 
 def zingNews():
-  url = 'https://zingnews.vn/'
-  req = requests.get(url)
-  html = req.content
-  soup = BeautifulSoup(html, 'html.parser')
+  soup = urlparsing('https://zingnews.vn/the-gioi.html')
+  url = 'https://zingnews.vn'
 
-  # 첫번째 기사 스크랩
-  main_title = soup.select_one('p.article-thumbnail > a > img')['alt']
-  main_image = soup.select_one('p.article-thumbnail > a > img')['src']
-  main_link = url + soup.select_one('p.article-thumbnail > a')['href']
-  # 서브 기사 스크랩
-  sub_items = soup.select('.article-title > a')
+  # 기사 스크랩
+  news = soup.select('p.article-thumbnail > a')
+  title = soup.select('p.article-title')
+  images = soup.select('.article-thumbnail > a > img')
+  link = soup.select('p.article-thumbnail > a')
 
-  news_title = [main_title]
-  news_img = [main_image]
-  news_link = [main_link]
+  # 이미지가 lazy loading 인경우 해당 이미지 attr 변경
+  # for i in images:
+  #   if i['data-src']:
+  #     i['src'] = i['data-src']
+  #     del i['data-src']
 
-  for news in sub_items:
-    news_title.append(news.get_text())
-    news_img.append('')
+  # 기사 넣을 빈 리스트생성
+  news_title, news_img, news_link = [], [], []
+
+  # 이걸로 대체하고싶지만 img부분에 url이 추가되어야함...
+  # news_title = [[news.text for news in title]]
+  # news_img = [news['src'] for news in image]
+  # news_link = [news['href'] for news in link]
+
+  # 각 리스트에 스크랩한 기사 추가
+  for news in title:
+    news_title.append(news.text)
+  for news in link:
     news_link.append(url + news['href'])
+  for news in images:
+    news_img.append(news['src'])
 
-  titleList = news_title[0:8]
-  imgList = news_img[0:8]
-  linkList = news_link[0:8]
-
-  merged = dict([x for x in zip(titleList, zip(linkList, imgList))])
+  # 딕셔너리화
+  merged = news_merge(news_title, news_img, news_link)
   return merged
 
-def tuoiTre():
-  url = 'https://tuoitre.vn/'
-  req = requests.get(url)
-  html = req.content
-  soup = BeautifulSoup(html, 'html.parser')
-
-  # 첫번째 기사 스크랩
-  main_title = soup.select_one('.focus-first > a > img')['alt']
-  main_image = soup.select_one('.focus-first > a > img')['src']
-  main_link = url + soup.select_one('.focus-first > a')['href']
-  # 서브 기사 스크랩
-  sub_items = soup.select('.title-name > a')
-
-  news_title = [main_title]
-  news_img = [main_image]
-  news_link = [main_link]
-
-  for news in sub_items:
-    news_title.append(news.get_text())
-    news_img.append('')
-    news_link.append(url + news['href'])
-
-  titleList = news_title[0:8]
-  imgList = news_img[0:8]
-  linkList = news_link[0:8]
-
-  merged = dict([x for x in zip(titleList, zip(linkList, imgList))])
-  return merged
-
-def thanhNien():
-  url = 'https://thanhnien.vn/'
-  req = requests.get(url)
-  html = req.content
-  soup = BeautifulSoup(html, 'html.parser')
-
-  # 첫번째 기사 스크랩
-  main_title = soup.select_one('.highlight > article > a')['title']
-  main_image = soup.select_one('.highlight > article > a > img')['src']
-  main_link = url + soup.select_one('.highlight > article > a')['href']
-  # 서브 기사 스크랩
-  sub_items = soup.select('.feature > article > h2 > a')
-
-  news_title = [main_title]
-  news_img = [main_image]
-  news_link = [main_link]
-
-  for news in sub_items:
-    news_title.append(news.get_text())
-    news_img.append('')
-    news_link.append(url + news['href'])
-
-  titleList = news_title[0:8]
-  imgList = news_img[0:8]
-  linkList = news_link[0:8]
-  merged = dict([x for x in zip(titleList, zip(linkList, imgList))])
-  return merged
+a = zingNews()
+print(a)
 
 def search_trends():
   url = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=VN'
@@ -146,7 +74,7 @@ def search_trends():
   # 데이터 찾기 (title, traffic)
   search_trend = soup.find_all('title')
   search_traffic = soup.find_all('ht:approx_traffic')
-  # 배열을 만들자
+  # 배열 생성
   trendList = []
   trafficList = []
   for i in search_trend:
